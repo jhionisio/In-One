11e61091f5d9b165afddc02d6836d6201e16d58d
Guilherme Ferreira <guilhermeferreira0711@gmail.com>
2023-04-16 02:05:08 -0300

    Endpoint1

--- src/main/java/in/one/in_one/exception/RestNotFoundException.java
+++ src/main/java/in/one/in_one/exception/RestNotFoundException.java
@@ -3,6 +3,7 @@ package in.one.in_one.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class RestNotFoundException extends RuntimeException {
 
