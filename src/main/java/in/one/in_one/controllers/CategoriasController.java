d4243d8384a0dd50b9cbb23d96fb84460b4ae536
jhionisio <mr.joaomarcelo@gmail.com>
2023-04-24 18:01:11 -0300

    DONE

--- src/main/java/in/one/in_one/controllers/CategoriasController.java
+++ src/main/java/in/one/in_one/controllers/CategoriasController.java
@@ -6,6 +6,12 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -30,7 +36,7 @@ public class CategoriasController {
     @Autowired // IoD IoC
     CategoriasRepository repository;
 
-    @GetMapping
+/*    @GetMapping
     public Page<Categoria> index(@RequestParam(required = false) String name, @PageableDefault(size = 5) Pageable pageable){
         if (name == null) return repository.findAll(pageable);
         return repository.findByDocsContaining(name, pageable);
@@ -48,6 +54,52 @@ public class CategoriasController {
         log.info("Buscando categoria com id " + id);
         return ResponseEntity.ok(getCategoria(id));
     }
+*/
+
+    @GetMapping
+    public ResponseEntity<CollectionModel<EntityModel<Categoria>>> index(@RequestParam(required = false) String name, @PageableDefault(size = 5) Pageable pageable){
+
+        List<EntityModel<Categoria>> categoriasModel = new ArrayList<>();
+
+        if (name == null) {
+            List<Categoria> categorias = repository.findAll(pageable).getContent();
+            for (Categoria categoria : categorias) {
+                categoriasModel.add(getCategoriaModel(categoria));
+            }
+        } else {
+            List<Categoria> categorias = repository.findByDocsContaining(name, pageable).getContent();
+            for (Categoria categoria : categorias) {
+                categoriasModel.add(getCategoriaModel(categoria));
+            }
+        }
+
+        CollectionModel<EntityModel<Categoria>> collectionModel = CollectionModel.of(categoriasModel);
+        collectionModel.add(getSelfLink());
+        return ResponseEntity.ok(collectionModel);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<EntityModel<Categoria>> create(@RequestBody @Valid Categoria categoria){
+        log.info("Cadastrando categoria: " + categoria);
+        Categoria postObj = repository.save(categoria);
+        EntityModel<Categoria> categoriaModel = getCategoriaModel(postObj);
+        categoriaModel.add(getSelfLink());
+        categoriaModel.add(getUpdateLink(postObj.getId()));
+        categoriaModel.add(getDeleteLink(postObj.getId()));
+        return ResponseEntity.created(categoriaModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(categoriaModel);
+    }
+    
+    @GetMapping("{id}")
+    public ResponseEntity<EntityModel<Categoria>> show(@PathVariable Long id){
+        log.info("Buscando categoria com id " + id);
+        Categoria categoria = getCategoria(id);
+        EntityModel<Categoria> categoriaModel = getCategoriaModel(categoria);
+        categoriaModel.add(getSelfLink());
+        categoriaModel.add(getUpdateLink(id));
+        categoriaModel.add(getDeleteLink(id));
+        return ResponseEntity.ok(categoriaModel);
+    }
 
     @DeleteMapping("{id}")
     public ResponseEntity<Categoria> destroy(@PathVariable Long id){
@@ -57,11 +109,14 @@ public class CategoriasController {
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<Categoria> update(@PathVariable Long id, @RequestBody @Valid Categoria categoria){
+    public ResponseEntity<EntityModel<Categoria>> update(@PathVariable Long id, @RequestBody @Valid Categoria categoria){
         log.info("Alterando categoria com id " + id);
         getCategoria(id);
         categoria.setId(id);
-        repository.save(categoria);
+        Categoria putObj = repository.save(categoria);
+        EntityModel<Categoria> categoriaModel = getCategoriaModel(putObj);
+        categoriaModel.add(getSelfLink());
+        categoriaModel.add(getDeleteLink(putObj.getId()));
         return ResponseEntity.ok(categoria);
     }
 
